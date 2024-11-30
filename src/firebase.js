ee194e03b72b69609b58d82a7e41a10eadb6a2fe
cieltsukishima <cieltsukishima@gmail.com>
2024-11-30 05:00:35 +0000

    db

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -11,3 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export { db }
